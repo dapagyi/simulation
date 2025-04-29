93870dfdad76a513a63d2518d5bdcb5fdd6bc056
David Apagyi <apagyi.david@gmail.com>
2025-04-29 03:08:44 +0300

    refactor: parameterize tests

--- tests/heuristics/test_n_queen.py
+++ tests/heuristics/test_n_queen.py
@@ -12,40 +12,38 @@ def test_permutation_initialization():
     assert len(set(board.queen_positions_per_row)) == 1000
 
 
-def test_unsolvable_board():
+@pytest.mark.parametrize("n", [2, 3])
+def test_unsolvable_board(n):
     max_steps = 100
 
-    for n in [2, 3]:
-        board = Chessboard.from_random_permutation(n)
-        solver = Solver(board)
-        with pytest.raises(RuntimeError, match="Failed to solve the board"):
-            solver.solve()
-        assert solver.status == SolverStatus.REACHED_MAX_NUMBER_OF_STEPS
-        assert solver.current_step == max_steps
-
-
-def test_n_queens():
-    for n in [4, 8, 16, 32, 64]:
-        max_steps = n * 100
-
-        board = Chessboard.from_random_permutation(n)
-        solver = Solver(board, max_steps=max_steps)
-        assert solver.status == SolverStatus.UNSOLVED
-        assert solver.current_step == 0
+    board = Chessboard.from_random_permutation(n)
+    solver = Solver(board)
+    with pytest.raises(RuntimeError, match="Failed to solve the board"):
         solver.solve()
+    assert solver.status == SolverStatus.REACHED_MAX_NUMBER_OF_STEPS
+    assert solver.current_step == max_steps
 
-        assert solver.status == SolverStatus.SOLVED
-        assert solver.current_step < max_steps
-        assert len(set(solver.board.queen_positions_per_row)) == n
 
-        # Test for conflicts
-        for row, col in enumerate(solver.board.queen_positions_per_row):
-            assert solver._count_conflicts_for_position(row, col) == 0
+@pytest.mark.parametrize("n", [4, 8, 16, 32, 64])
+def test_n_queens(n):
+    max_steps = n * 100
 
-        # Test conflicts manually
-        for i in range(n):
-            for j in range(i + 1, n):
-                assert solver.board.queen_positions_per_row[i] != solver.board.queen_positions_per_row[j]
-                assert abs(i - j) != abs(
-                    solver.board.queen_positions_per_row[i] - solver.board.queen_positions_per_row[j]
-                )
+    board = Chessboard.from_random_permutation(n)
+    solver = Solver(board, max_steps=max_steps)
+    assert solver.status == SolverStatus.UNSOLVED
+    assert solver.current_step == 0
+    solver.solve()
+
+    assert solver.status == SolverStatus.SOLVED
+    assert solver.current_step < max_steps
+    assert len(set(solver.board.queen_positions_per_row)) == n
+
+    # Test for conflicts
+    for row, col in enumerate(solver.board.queen_positions_per_row):
+        assert solver._count_conflicts_for_position(row, col) == 0
+
+    # Test conflicts manually
+    for i in range(n):
+        for j in range(i + 1, n):
+            assert solver.board.queen_positions_per_row[i] != solver.board.queen_positions_per_row[j]
+            assert abs(i - j) != abs(solver.board.queen_positions_per_row[i] - solver.board.queen_positions_per_row[j])
